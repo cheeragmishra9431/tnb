3c5286ae2cce7e36cb88c1d6413cbc1dca59e9cb
cheeragmishra9431 <waytogocheerag@gmail.com>
2022-02-09 11:09:58 +0530

    tested the get("/") endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,14 @@
 const express=require("express");
 const router=express.Router();
+const User=require('../models/User');
+//Create a user using: POST "/api/auth/" . doesnt requirea auth
 
 router.get('/',(req,res)=>{
-    obj={
-        a:"ssdvd",
-        s:45
-    }
-    res.json(obj)
+   console.log(req.body);
+   const user=User(req.body);
+   user.save()
+   res.send(req.body);
+    // res.json(obj)
     
 })
 module.exports=router
