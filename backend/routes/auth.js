d86fa8ee125fd8bc947c0145f9bfc4c027a886c9
cheeragmishra9431 <waytogocheerag@gmail.com>
2022-02-09 11:50:03 +0530

    Made create indexes used body validation and used then and catch

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,25 @@
 const express=require("express");
 const router=express.Router();
 const User=require('../models/User');
-//Create a user using: POST "/api/auth/" . doesnt requirea auth
+const { body, validationResult } = require('express-validator');
 
-router.get('/',(req,res)=>{
-   console.log(req.body);
-   const user=User(req.body);
-   user.save()
-   res.send(req.body);
+//Create a user using: POST "/api/auth/" . doesnt requirea auth
+router.get('/',[body('email',"Enter a valid email").isEmail(),
+    body('name',"Enter a valid name").isLength({min:3}),
+    body('password',"Enter a valid password").isEmail({min:5}),],
+    (req,res)=>{
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+     return res.status(400).json({ errors: errors.array() });
+   }
+   User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  }).then(user => res.json(user)).catch(err=>{console.log(err);
+    res.json({error:"please enter a unique value for email", message:err.message})})
+
+ 
     // res.json(obj)
     
 })
